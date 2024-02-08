f467e80e5515a8405d866d4b3b0add1827140e18
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-08 13:44:16 +0530

    Print_Byte_Variable123

---
+++ Print_Byte_Variable123.java
@@ -0,0 +1,24 @@
+//first  print of  byte variable 
+
+class Print_Byte_Variable123
+
+{
+
+//This is main method
+public static void main(String [] pbv)
+{
+//declaration and intialization of byte  variable 
+
+byte variable1=20;
+
+//printing byte variable 
+
+System.out.println(variable1);
+
+}
+
+}
+/*output:
+1:Compile time:Success
+2:RunTime:Success: 20
+*/
