c8e818a3a5869bcf63a039699e3e47539242a200
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-09 11:07:06 +0530

    Operator

---
+++ Operator.java
@@ -0,0 +1,16 @@
+class Operator
+{
+public static void main(String []o)
+{ 
+int a =10, b =20, c=30;
+int operator1 = ++a -a+++b-- -b+c +a +a+ --c+a+--b+ --c;
+int operator2 = c+c+c+ ++c+b- --b+a-b+c+ --b+ ++c;
+
+System.out.println("operator1="+operator1);
+System.out.println("operator2="+operator2);
+
+
+
+}
+
+}
